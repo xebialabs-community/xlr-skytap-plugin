67faab2fb988ce629cf97b3a9c1a107591dd86a9
Erik Rasmussen <erasmussen@xebialabs.com>
2017-08-07 08:45:09 -0600

    Checkpoint.

--- src/main/resources/skytap/Skytap.py
+++ src/main/resources/skytap/Skytap.py
@@ -1,4 +1,4 @@
-#
+    #
 # Copyright 2017 XEBIALABS
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -40,30 +40,31 @@ class SkytapClient(object):
             full_endpoint_url = "%s/%s" % (full_endpoint_url, object_id)
         response = self.open_url(method, full_endpoint_url, headers=headers, json_data=json_data, data=data)
         if not response.status_code == 200:
+            print "response.text: %s" % response.text
             raise Exception(error_message)
         return response.text
 
     def skytap_userlist(self, variables):
-        return self.get_response_for_endpoint("GET", "users", "Failed to retrieve user list.")
+        return {"output" : self.get_response_for_endpoint("GET", "users", "Failed to retrieve user list.")}
 
     def skytap_projectlist(self, variables):
-        return self.get_response_for_endpoint("GET", "projects", "Failed to retrieve project list.")
+        return {"output" : self.get_response_for_endpoint("GET", "projects", "Failed to retrieve project list.")}
 
     def skytap_templatelist(self, variables):
-        return self.get_response_for_endpoint("GET", "templates", "Failed to retrieve template list.")
+        return {"output" : self.get_response_for_endpoint("GET", "templates", "Failed to retrieve template list.")}
 
     def skytap_environmentlist(self, variables):
-        return self.get_response_for_endpoint("GET", "configurations", "Failed to retrieve environment list.")
+        return {"output" : self.get_response_for_endpoint("GET", "configurations", "Failed to retrieve environment list.")}
 
     def skytap_getenvironment(self, variables):
-        return self.get_response_for_endpoint("GET", "configurations", "Failed to retrieve environment [%s]." % variables['environment_id'], object_id=variables['environment_id'])
+        return {"output" : self.get_response_for_endpoint("GET", "configurations", "Failed to retrieve environment [%s]." % variables['environment_id'], object_id=variables['environment_id'])}
 
     def skytap_environmentvmlist(self, variables):
         json_data = json.loads(self.skytap_getenvironment(variables))
-        return json.dumps(json_data['vms'])
+        return {"output" : json.dumps(json_data['vms'])}
 
     def skytap_assetlist(self, variables):
-        return self.get_response_for_endpoint("GET", "assets", "Failed to retrieve asset list.")
+        return {"output" : self.get_response_for_endpoint("GET", "assets", "Failed to retrieve asset list.")}
 
     def skytap_startenvironment(self, variables):
         if self.environment_runstate(variables, "running"):
@@ -71,14 +72,14 @@ class SkytapClient(object):
         while self.environment_runstate(variables, "busy"):
             time.sleep(5)
         data = '{"runstate": "running"}'
-        return self.get_response_for_endpoint("PUT", "configurations/%s.json" % variables['environment_id'], "Failed to start environment [%s]." % variables['environment_id'], data=data)
+        return {"output" : self.get_response_for_endpoint("PUT", "configurations/%s.json" % variables['environment_id'], "Failed to start environment [%s]." % variables['environment_id'], data=data)}
 
     def skytap_waitforrunningenvironment(self, variables):
         while True:
             if self.environment_runstate(variables, "running"):
                 break
             time.sleep(10)
-        return self.skytap_getenvironment(variables)
+        return {"output" : self.skytap_getenvironment(variables)}
 
     def skytap_createproject(self, variables):
         data = '{"name":"%s"}' % variables['project_name']
@@ -91,22 +92,24 @@ class SkytapClient(object):
         data = '{"template_id":"%s"' % variables['template_id']
         if variables['project_id'] is not None and ['project_id']:
             data += ', "project_id":"%s"' % variables['project_id']
-        data += '}'
-        return self.perform_post_operation(endpoint="configurations.json",
+        data += ', "public_ip_count" : 1}'
+        response = self.perform_post_operation(endpoint="configurations.json",
                                                 error_message="Failed to create environment with template [%s]." % variables['template_id'],
                                                 data=data)
+        return {"output" : "%s" % json.loads(response)['id']}
 
     def skytap_deleteproject(self, variables):
-        return self.perform_delete_operation(endpoint="projects", object_id=variables['project_id'])
+        self.perform_delete_operation(endpoint="projects", object_id=variables['project_id'])
+        return {"output" : "SUCCESS"}
 
     def skytap_deleteenvironment(self, variables):
-        return self.perform_delete_operation(endpoint="configurations", object_id=variables['environment_id'])
+        return {"output" : self.perform_delete_operation(endpoint="configurations", object_id=variables['environment_id'])}
 
     def skytap_addtemplatetoproject(self, variables):
-        return self.perform_add_to_project_operation("templates", variables['project_id'], variables['template_id'])
+        return {"output" : self.perform_add_to_project_operation("templates", variables['project_id'], variables['template_id'])}
 
     def skytap_addenvironmenttoproject(self, variables):
-        return self.perform_add_to_project_operation("configurations", variables['project_id'], variables['environment_id'])
+        return {"output" : self.perform_add_to_project_operation("configurations", variables['project_id'], variables['environment_id'])}
 
     def perform_post_operation(self, endpoint, error_message, object_id=None, json_data=None, data=None, headers=None):
         return self.get_response_for_endpoint(
@@ -132,4 +135,4 @@ class SkytapClient(object):
         self.perform_post_operation(endpoint=full_endpoint, error_message="Failed to add %s [%s] to project [%s]." % (endpoint, object_id, project_id))
 
     def environment_runstate(self, variables, runstate):
-        return json.loads(self.skytap_getenvironment(variables))['runstate'] == runstate
+        return json.loads(self.skytap_getenvironment(variables)['output'])['runstate'] == runstate
